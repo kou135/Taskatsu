dc8b433825c73a8de9a33a6dd759e18447639d7d
kou135 <koa58550@gmail.com>
2025-07-03 03:13:06 +0900

    google認証系実装

---
+++ auth.ts
@@ -0,0 +1,16 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope: "openid email profile https://www.googleapis.com/auth/gmail.send",
+        },
+      },
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+};
